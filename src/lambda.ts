1b3f0123475b786d91a7057740f61519f5c7bdc7
Thiago Lage <tdlage@gmail.com>
2025-07-03 21:53:28 -0300

    Update to serverless-http and ESNEXT TypeScript target as recommended

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,4 +1,4 @@
-import serverlessExpress from '@codegenie/serverless-express'
+import serverless from 'serverless-http'
 import { app } from './app'
 
-export const handler = serverlessExpress({ app })
+export const handler = serverless(app)
